5948aa00b6701aa9c0810f62396ab7af38215aa0
Frank712 <franksuriel_7@hotmail.com>
2019-01-24 18:08:26 -0600

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PortafolioComponent } from './portafolio/portafolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavMenuComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    PortafolioComponent
   ],
   imports: [
     BrowserModule
